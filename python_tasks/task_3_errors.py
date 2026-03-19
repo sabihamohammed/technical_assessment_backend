b9dd2c4258d56b138b735a0b1b66bc8b80176918
Sabiha <101255170+sabihamohammed@users.noreply.github.com>
2026-03-19 15:51:38 -0400

    Implement input validation in calculate_discount function
    Added error handling for invalid inputs in calculate_discount function.

--- python_tasks/task_3_errors.py
+++ python_tasks/task_3_errors.py
@@ -6,7 +6,18 @@ def calculate_discount(price, discount_percent):
     Instructions: Handle cases where discount_percent is 0 
     or if inputs are strings/None. Return 0 for invalid inputs.
     """
-    # TODO: Implement logic
+    try:
+        if price is None or discount_percent is None:
+            return 0
+        
+        price = float(price)
+        discount_percent= float(discount_percent)
+
+        if discount_percent == 0:
+            return 0
+        return price * discount_percent/100
+    except(ValueError,TypeError):
+        return 0
     
 
 # Test Case
