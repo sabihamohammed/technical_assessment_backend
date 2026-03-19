cb9d31f13ba3b2deecdfa272f364b0d573c83bf8
pa-elijah <elijah.behizadi@partsavatar.ca>
2026-03-19 01:55:56 -0400

    Initial setup

--- python_tasks/task_3_errors.py
+++ python_tasks/task_3_errors.py
@@ -6,11 +6,8 @@ def calculate_discount(price, discount_percent):
     Instructions: Handle cases where discount_percent is 0 
     or if inputs are strings/None. Return 0 for invalid inputs.
     """
-    try:
-        # TODO: Implement logic
-        return price * (discount_percent / 100)
-    except (ZeroDivisionError, TypeError):
-        return 0
+    # TODO: Implement logic
+    
 
 # Test Case
 print(calculate_discount(100, "10")) # Should return 0 or handle conversion
